12df0f4863556406effbbe345fc5c9aa9c7559ed
seethara666 <70530913+seethara666@users.noreply.github.com>
2022-04-02 15:52:26 -0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Student extends account {
+    public static ArrayList<String> quizGrades = new ArrayList<>();
+
+    public Student(String userName) {
+        super(userName);
+    }
+
+
+    public boolean readGrades() {
+        try {
+            File f1 = new File((getUserName() + ".txt"));
+            FileReader fr1 = new FileReader(f1);
+            BufferedReader bfr = new BufferedReader(fr1);
+            String tempLine;
+            while ((tempLine = bfr.readLine()) != null) {
+                    quizGrades.add(tempLine);
+            }
+            String[] quizDetails = new String[2];
+            for(int i = 0; i < quizGrades.size(); i++) {
+                quizDetails = quizGrades.get(i).split(",");
+                System.out.println(quizDetails[0] + " - " + quizDetails[1]);
+            }
+        } catch (IOException e) {
+            //NoGradesFoundException();
+            return false;
+        }
+        return true;
+    }
+
+
+    public boolean addGrade(String quizName, String quizGrade) {
+        try {
+            File f1 = new File((getUserName() + ".txt"));
+            FileWriter fr1 = new FileWriter(f1, true);
+            BufferedWriter bfw = new BufferedWriter(fr1);
+            bfw.write((quizName + "," + quizGrade + "\n"));
+            bfw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+}
