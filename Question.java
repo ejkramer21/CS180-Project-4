73089791b9132760e3c85649be76ef0215fa0609
kiichigojam <52803247+kiichigojam@users.noreply.github.com>
2022-04-06 21:16:03 -0400

    Add files via upload

--- Question.java
+++ Question.java
@@ -1,28 +1,40 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Question {
     private String question;
-    private String option;
+    private String[] option;
     private int answer;
     private int numQuestions;
     private ArrayList<String> questions;
-    private Stringp[] question;
+    private int integerPointValue;
 
 
-    public Question(String question, String[4] option, int answer) {
+    public Question(String question, String[] option, int answer) {
         this.question = question;
         this.option = option;
         this.answer = answer;
+        this.questions = new ArrayList<String>();
     }
 
-    public Question(String question, String[4] option) {
+    public int getIntegerPointValue(){
+        return this.integerPointValue;
+    }
+
+    public void setIntegerPointValue(int integerPointValue){
+        this.integerPointValue = integerPointValue;
+    }
+
+    public Question(String question, String[] option) {
         this.question = question;
         this.option = option;
+        this.questions = new ArrayList<String>();
     }
 
-    public String[] numQuestions(String line, String filename) {
+    public String[] numQuestions(String line, String filename) throws IOException {
         int lineCount = 0;
         File f = new File(filename);
         BufferedReader bfr = new BufferedReader(new FileReader(f));
@@ -30,13 +42,13 @@ public class Question {
             lineCount++;
             questions.add(line);
         }
-        question = new String[questions.size()/5];
+        String[] questionInfo = new String[questions.size()/5];
         for (int i = 0; i < questions.size(); i++) {
             if (i % 5 != 0) {
-                question[i] = questions.get(i);
+                questionInfo[i] = questions.get(i);
             }
         }
-        return question;
+        return questionInfo;
     }
 
 
