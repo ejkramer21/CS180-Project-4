09791d6ce90e8a3f8da342a2d3838993eabe36a6
seethara666 <70530913+seethara666@users.noreply.github.com>
2022-04-09 16:51:31 -0400

    Add files via upload
    Student changes + joy's constructor

--- Student.java
+++ Student.java
@@ -1,12 +1,10 @@
 import java.io.*;
 import java.util.ArrayList;
-/* Student class extends account
- * you can add a grade or get grades for a particular student
- */
 
-//capitalized account-Joy
-public class Student extends Account {
-    public static ArrayList<String> quizGrades = new ArrayList<>();
+public class Student extends account {
+    public ArrayList<String> quizGrades = new ArrayList<>();
+    public ArrayList<String> quizNames = new ArrayList<>();
+    public ArrayList<String> quizTotal = new ArrayList<>();
 
     public Student(String username, String password, String userType) {
         //I added the password and userType to work with the constructor of Account
@@ -17,7 +15,9 @@ public class Student extends Account {
         super(username, password, userType);
     }
 
-
+    /*
+        disclaimer: readGrades is not updated.
+     */
     public boolean readGrades() throws NoGradesFoundException {
         try {
             File f1 = new File((getUserName() + ".txt"));
@@ -25,35 +25,58 @@ public class Student extends Account {
             BufferedReader bfr = new BufferedReader(fr1);
             String tempLine;
             while ((tempLine = bfr.readLine()) != null) {
-                quizGrades.add(tempLine);
+                quizNames.add(tempLine);
+                quizGrades.add(bfr.readLine());
+                quizTotal.add(bfr.readLine());
             }
-            String[] quizDetails = new String[2];
-            for(int i = 0; i < quizGrades.size(); i++) {
+            String[] quizDetails;
+            for (int i = 0; i < quizGrades.size(); i++) {
+                System.out.println(quizNames.get(i));
                 quizDetails = quizGrades.get(i).split(",");
-                System.out.println(quizDetails[0] + " - " + quizDetails[1]);
+                for (int j = 0; j < quizDetails.length / 2; j++) {
+                    System.out.println("Question " + quizDetails[j] + " - " + quizDetails[j + 1]);
+                }
+
             }
         } catch (IOException e) {
             throw new NoGradesFoundException("This student has no grades!");
-            return false;
         }
         return true;
     }
 
+    /*
+        Student.addGrade HOW TO USE
+        This function will store the grades for each question for a quiz the student has taken. It'll also calculate
+        the total grade as a percentage.
+        When you finish grading the quizzes, you need to call this function to store the grade.
+        You need to call it with the student's name.addGrade(quizName, questionValue, questionGrade);
+        quizName is a String of the quiz's name
+        questionValue is an int array of the question point values
+        questionGrade is an int array of the points the student earns on each question
+        It will throw an exception if it fails.
 
-    public boolean addGrade(String quizName, String quizGrade) {
+    */
+    public void addGrade(String quizName, int[] questionValue, int[] questionGrade) throws InvalidAccountException {
+        int totalPoints = 0;
+        int totalGrade = 0;
+        double finalGrade;
         try {
             File f1 = new File((getUserName() + ".txt"));
             FileWriter fr1 = new FileWriter(f1, true);
             BufferedWriter bfw = new BufferedWriter(fr1);
-            bfw.write((quizName + "," + quizGrade + "\n"));
+            bfw.write((quizName + "\n"));
+            for (int i = 0; i < questionGrade.length; i++) {
+                bfw.write(questionValue[i] + "," + questionGrade[i]);
+                totalPoints += questionValue[i];
+                totalGrade += questionGrade[i];
+            }
+            finalGrade = totalGrade / totalPoints;
+            bfw.write("\n" + String.format("%.2f", finalGrade) + "\n");
             bfw.close();
         } catch (IOException e) {
-            e.printStackTrace();
-            return false;
+            throw new InvalidAccountException("Something went wrong!");
         }
-        return true;
     }
-
 
 
 }
