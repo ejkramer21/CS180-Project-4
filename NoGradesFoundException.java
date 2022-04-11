ecd2264b51950f387a4ef8e50fd6697813bcbe52
seethara666 <70530913+seethara666@users.noreply.github.com>
2022-04-11 00:00:59 -0400

    Update NoGradesFoundException.java

--- NoGradesFoundException.java
+++ NoGradesFoundException.java
@@ -1,3 +1,9 @@
+/**
+ * NoGradesFoundException
+ * An exception to be thrown when no grades are found for a student
+ * @author Ananya Seetharaman
+ * @version April 10, 2022
+ */
 public class NoGradesFoundException extends Exception {
         NoGradesFoundException(String message) {
             super(message);
