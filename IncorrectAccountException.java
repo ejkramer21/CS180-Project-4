84c00a24dc9768f56f0671cb117288a55014e9f6
seethara666 <70530913+seethara666@users.noreply.github.com>
2022-04-11 00:04:16 -0400

    Update IncorrectAccountException.java

--- IncorrectAccountException.java
+++ IncorrectAccountException.java
@@ -1,3 +1,10 @@
+/**
+ * IncorrectAccountException.java
+ * an exception to be thrown when the wrong type of account is used for something
+ *
+ * @author ?
+ * @version April 10, 2022
+ */
 public class IncorrectAccountException extends Exception {
     IncorrectAccountException(String message) {
         super(message);
