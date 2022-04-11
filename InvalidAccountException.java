a09fdbaa68535c5ff785ba16cb0896a9abb90654
seethara666 <70530913+seethara666@users.noreply.github.com>
2022-04-11 00:03:24 -0400

    Update InvalidAccountException.java

--- InvalidAccountException.java
+++ InvalidAccountException.java
@@ -1,3 +1,9 @@
+/**
+ * InvalidAccountException
+ * An exception to be thrown when an invalid account type is used
+ * @author Joy Kramer
+ * @version April 10, 2022
+ */
 public class InvalidAccountException extends Exception {
     InvalidAccountException(String message) {
         super(message);
