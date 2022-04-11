dffcc0cee8a5ab978c7e7e5507f87ec062c70bfa
seethara666 <70530913+seethara666@users.noreply.github.com>
2022-04-11 00:02:08 -0400

    Update QuizAlreadyExistsException.java

--- QuizAlreadyExistsException.java
+++ QuizAlreadyExistsException.java
@@ -1,3 +1,10 @@
+/**
+ * QuizAlreadyExistsException
+ *  An exception to be thrown when an attempt is made to create a quiz that already exists.
+ *
+ * @author Joy Kramer
+ * @version April 10, 2022
+ */
 public class QuizAlreadyExistsException extends Exception {
     public QuizAlreadyExistsException(String message) {
         super(message);
